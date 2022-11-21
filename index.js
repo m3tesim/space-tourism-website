9e8429cd4b2b0d72db90b20bbfd9d2deab4e5609
Moatasem <m3tsmsh@gmail.com>
2022-11-22 00:41:12 +0200

    finishing the website functionality and design

--- index.js
+++ index.js
@@ -15,13 +15,12 @@ menueIcon.addEventListener("click", () => {
   }
 });
 
-function setPage(element){
- // console.log("nav clicked",element);
-const tabs= document.querySelectorAll(" nav li")
-tabs.forEach(tab=>(tab.classList.remove("active")))
-//console.log("classes",tabs);
-element.classList.add("active")
-
+function setPage(element) {
+  // console.log("nav clicked",element);
+  const tabs = document.querySelectorAll(" nav li");
+  tabs.forEach((tab) => tab.classList.remove("active"));
+  //console.log("classes",tabs);
+  element.classList.add("active");
 }
 // Destination page
 function setDestination(name) {
@@ -46,6 +45,48 @@ function setCrew(name) {
   document.getElementById("crew-role").textContent = `${member.role}`;
   document.getElementById("crew-name").textContent = `${member.name}`;
   document.getElementById("crew-bio").textContent = `${member.bio}`;
+}
+
+//technology page
+const activeTech =document.querySelector("button[aria-selected=true]").name
+//console.log(activeTech,"active texh")
+resizetechImage()
+window.addEventListener("resize", resizetechImage);
+
+function resizetechImage(){
+
+  console.log("this active ",activeTech)
+  const tech = data["technology"].filter((i) => i.name === activeTech)[0];
+  document
+  if(window.innerWidth<=770){
+    document.getElementById("tech-img").src = `${tech.images.landscape}`;
+  }else{
+    document.getElementById("tech-img").src = `${tech.images.portrait}`
+
+  
+  }
+}
+
+
+function setTech(name) {
+  console.log(name,"from tech ");
+  const tech = data["technology"].filter((i) => i.name === name)[0];
+  document
+    .querySelectorAll("button[aria-selected]")
+    .forEach((i) => i.setAttribute("aria-selected", false));
+ document.getElementById(`${name}`).setAttribute("aria-selected", true);
+
+  document.getElementById("tech-name").textContent = `${tech.name}`;
+  document.getElementById("tech-description").textContent = `${tech.description}`;
+  console.log(window.innerWidth,"inner width from innerwidth")
+  if(window.innerWidth<=770){
+    document.getElementById("tech-img").src = `${tech.images.landscape}`;
+  
+  }else{
+    document.getElementById("tech-img").src = `${tech.images.portrait}`;
+
+  }
+  
 }
 
 /*
